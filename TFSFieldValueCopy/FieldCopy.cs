c6e9fbb646f289bcebb0d8cfa46dd700b708ed00
Marc Müller <mmueller@4tecture.ch>
2015-02-16 16:41:49 +0100

    Initial Commit

---
+++ TFSFieldValueCopy/FieldCopy.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.TeamFoundation.Client;
+using Microsoft.TeamFoundation.WorkItemTracking.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TFSFieldValueCopy
+{
+    public static class FieldCopy
+    {
+        public static void CopyFieldValues(string tfsName, string[] tfsProjectNames, string[] tfsWorkItemTypes, string fromField, string toField, bool deleteFromValue = false)
+        {
+            using (TeamFoundationServer tfs = TeamFoundationServerFactory.GetServer(tfsName))
+            {
+                WorkItemStore wit = (WorkItemStore)tfs.GetService(typeof(WorkItemStore));
+                WorkItemCollection result = wit.Query(String.Format("SELECT [System.Id], [{0}], [{1}] FROM WorkItems WHERE [System.TeamProject] IN ({2}) AND [System.WorkItemType] IN ({3})", fromField, toField, string.Join(", ", tfsProjectNames.Select(s => "'" + s + "'")), string.Join(", ", tfsWorkItemTypes.Select(s => "'" + s + "'"))));
+                List<WorkItem> affectedWorkItems = new List<WorkItem>();
+                foreach (WorkItem wi in result)
+                {
+                    try
+                    {
+                        var fromValue = wi[fromField];
+                        if (fromValue != null)
+                        {
+                            wi.Open();
+                            var orig = wi[toField];
+                            wi[toField] = fromValue;
+
+                            if (deleteFromValue)
+                            {
+                                wi[fromField] = null;
+                            }
+
+                            affectedWorkItems.Add(wi);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Could not copy field value for item {0}: {1}", wi.Id, ex.Message);
+                    }
+                }
+
+                if (affectedWorkItems.Count > 0)
+                {
+                    wit.BatchSave(affectedWorkItems.ToArray());
+                    Console.WriteLine("Items updated: " + affectedWorkItems.Count);
+                }
+                else
+                {
+                    Console.WriteLine("Nothing to do.");
+                }
+            }
+        }
+    }
+}
---
+++ TFSFieldValueCopy/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TFSFieldValueCopy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 5)
+            {
+                var tfsServer = args[0];
+                var tfsProjectNames = args[1].Split(',');
+                var tfsWorkItemTypes = args[2].Split(',');
+                var fromField = args[3];
+                var toField = args[4];
+
+                Console.WriteLine("TFS Server: {0}", tfsServer);
+                Console.WriteLine("Projects: {0}", string.Join(", ", tfsProjectNames.Select(s => "'"+s+"'")));
+                Console.WriteLine("WITs: {0}", string.Join(", ", tfsWorkItemTypes.Select(s => "'" + s + "'")));
+                Console.WriteLine("From Field: {0}", fromField);
+                Console.WriteLine("To Field: {0}", toField);
+
+                Console.WriteLine("Continue?");
+                var confirm = Console.ReadLine();
+                if (confirm.ToLowerInvariant().StartsWith("y"))
+                {
+                    FieldCopy.CopyFieldValues(tfsServer, tfsProjectNames, tfsWorkItemTypes, fromField, toField, false);
+                }
+                else
+                {
+                    Console.WriteLine("Cancelled!");
+                }
+                Console.WriteLine("<ENTER> to exit?");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Wrong input");
+            }
+        }
+    }
+}
