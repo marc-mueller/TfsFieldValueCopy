c3e14ffb00c5bc00f66900b75a4b0b1e7e3fcf69
Marc Müller <mmueller@4tecture.ch>
2016-04-22 08:47:18 +0200

    added support to combine multiple fromfields into targetfield changed command line parameter handling

--- TFSFieldValueCopy/FieldCopy.cs
+++ TFSFieldValueCopy/FieldCopy.cs
@@ -8,12 +8,17 @@ namespace TFSFieldValueCopy
 {
     public static class FieldCopy
     {
-        public static void CopyFieldValues(Uri teamProjectCollectionUri, string[] tfsProjectNames, string[] tfsWorkItemTypes, string fromField, string toField, bool deleteFromValue = false, Func<object, bool> isHistoryValueMatching = null)
+        public static void CopyFieldValues(Uri teamProjectCollectionUri, string[] tfsProjectNames, string[] tfsWorkItemTypes, string[] fromFields, string toField, bool deleteFromValue = false, Func<object, bool> isHistoryValueMatching = null)
         {
             using (TfsTeamProjectCollection tfs = new TfsTeamProjectCollection(teamProjectCollectionUri))
             {
                 WorkItemStore wit = tfs.GetService<WorkItemStore>();
-                WorkItemCollection result = wit.Query(String.Format("SELECT [System.Id], [{0}], [{1}] FROM WorkItems WHERE [System.TeamProject] IN ({2}) AND [System.WorkItemType] IN ({3})", fromField, toField, string.Join(", ", tfsProjectNames.Select(s => "'" + s + "'")), string.Join(", ", tfsWorkItemTypes.Select(s => "'" + s + "'"))));
+                WorkItemCollection result = wit.Query(String.Format("SELECT [System.Id], {0}, [{1}] FROM WorkItems WHERE [System.TeamProject] IN ({2}) AND [System.WorkItemType] IN ({3})", 
+                    string.Join(", ",fromFields.Select(s => $"[{s}]")), 
+                    toField, 
+                    string.Join(", ", tfsProjectNames.Select(s => "'" + s + "'")), 
+                    string.Join(", ", tfsWorkItemTypes.Select(s => "'" + s + "'"))
+                    ));
                 List<WorkItem> affectedWorkItems = new List<WorkItem>();
                 foreach (WorkItem wi in result)
                 {
@@ -22,9 +27,13 @@ namespace TFSFieldValueCopy
                         object fromValue = null;
                         if (isHistoryValueMatching != null)
                         {
+                            if(fromFields.Length != 1)
+                            {
+                                throw new Exception("History value matching only works with a single from field!");
+                            }
                             foreach(Revision revision in wi.Revisions.OfType<Revision>().OrderByDescending(o => o.Index))
                             {
-                                var historyValue = revision.Fields[fromField].Value;
+                                var historyValue = revision.Fields[fromFields[0]].Value;
                                 if (isHistoryValueMatching(historyValue))
                                 {
                                     fromValue = historyValue;
@@ -33,7 +42,33 @@ namespace TFSFieldValueCopy
                             }
                         }
                         else {
-                            fromValue = wi[fromField];
+                            if (fromFields.Length == 1)
+                            {
+                                fromValue = wi[fromFields[0]];
+                            }
+                            else
+                            {
+                                string separator = "";
+                                if (wi.Fields[toField].FieldDefinition.FieldType == FieldType.PlainText)
+                                {
+                                    separator = "\r\n\r\n";
+                                }
+                                else if (wi.Fields[toField].FieldDefinition.FieldType == FieldType.Html)
+                                {
+                                    separator = "<br />";
+                                }
+                                else
+                                {
+                                    throw new Exception("Multiple fields can only be combined into a text based target field!");
+                                }
+
+                                List<string> fromValues = new List<string>();
+                                foreach (var fromField in fromFields)
+                                {
+                                   fromValues.Add(wi[fromField].ToString());
+                                }
+                                fromValue = string.Join(separator, fromValues);
+                            }
                         }
 
                         if (fromValue != null)
@@ -44,7 +79,10 @@ namespace TFSFieldValueCopy
 
                             if (deleteFromValue)
                             {
-                                wi[fromField] = null;
+                                foreach (var fromField in fromFields.Where(f => !f.Equals(toField, StringComparison.InvariantCultureIgnoreCase)))
+                                {
+                                    wi[fromField] = null;
+                                }
                             }
 
                             affectedWorkItems.Add(wi);
--- TFSFieldValueCopy/Program.cs
+++ TFSFieldValueCopy/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Fclp;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,47 +11,56 @@ namespace TFSFieldValueCopy
     {
         static void Main(string[] args)
         {
-            if (args.Length == 5)
+
+            string tfsCollectionUri = string.Empty;
+            string[] tfsProjectNames = new string[0];
+            string[] tfsWorkItemTypes = new string[0];
+            string[] fromFields = new string[0];
+            string toField = string.Empty;
+            bool deleteFromFieldValue = false;
+
+            var parser = new FluentCommandLineParser();
+            parser.Setup<string>('c', "collection").Callback(p => tfsCollectionUri = p).Required();
+            parser.Setup<string>('p', "projectNames").Callback(p => tfsProjectNames = p.Split(',')).Required();
+            parser.Setup<string>('w', "workItemTypes").Callback(p => tfsWorkItemTypes = p.Split(',')).Required();
+            parser.Setup<string>('f', "fromFields").Callback(p => fromFields = p.Split(',')).Required();
+            parser.Setup<string>('t', "toField").Callback(p => toField = p).Required();
+            parser.Setup<bool>('d', "deleteFromFieldValue").Callback(p => deleteFromFieldValue = p);
+
+            parser.SetupHelp("?", "help").Callback(text => Console.WriteLine(text));
+
+            parser.Parse(args);
+
+
+            Console.WriteLine("TFS Team Project Collection Uri: {0}", tfsCollectionUri);
+            Console.WriteLine("Projects: {0}", string.Join(", ", tfsProjectNames.Select(s => "'" + s + "'")));
+            Console.WriteLine("WITs: {0}", string.Join(", ", tfsWorkItemTypes.Select(s => "'" + s + "'")));
+            Console.WriteLine("From Field: {0}", string.Join(", ",fromFields));
+            Console.WriteLine("To Field: {0}", toField);
+
+            Console.WriteLine("Continue?");
+            var confirm = Console.ReadLine();
+            if (confirm.ToLowerInvariant().StartsWith("y"))
             {
-                var tfsCollectionUri = args[0];
-                var tfsProjectNames = args[1].Split(',');
-                var tfsWorkItemTypes = args[2].Split(',');
-                var fromField = args[3];
-                var toField = args[4];
-
-                Console.WriteLine("TFS Team Project Collection Uri: {0}", tfsCollectionUri);
-                Console.WriteLine("Projects: {0}", string.Join(", ", tfsProjectNames.Select(s => "'"+s+"'")));
-                Console.WriteLine("WITs: {0}", string.Join(", ", tfsWorkItemTypes.Select(s => "'" + s + "'")));
-                Console.WriteLine("From Field: {0}", fromField);
-                Console.WriteLine("To Field: {0}", toField);
-
-                Console.WriteLine("Continue?");
-                var confirm = Console.ReadLine();
-                if (confirm.ToLowerInvariant().StartsWith("y"))
+                if (Uri.IsWellFormedUriString(tfsCollectionUri, UriKind.Absolute))
                 {
-                    if (Uri.IsWellFormedUriString(tfsCollectionUri, UriKind.Absolute))
-                    {
-                        FieldCopy.CopyFieldValues(new Uri(tfsCollectionUri), tfsProjectNames, tfsWorkItemTypes, fromField, toField, false);
-                        
-                        // sample for providing a history value matching lambda function to get the right history value (newest will be checked first
-                        //FieldCopy.CopyFieldValues(new Uri(tfsCollectionUri), tfsProjectNames, tfsWorkItemTypes, fromField, toField, false, val => val != null && long.Parse(val.ToString()) <= 1000);
-                    }
-                    else
-                    {
-                        Console.WriteLine("Team Project Collection Uri is not well-formed!");
-                    }
+                    FieldCopy.CopyFieldValues(new Uri(tfsCollectionUri), tfsProjectNames, tfsWorkItemTypes, fromFields, toField, deleteFromFieldValue);
+
+                    // sample for providing a history value matching lambda function to get the right history value (newest will be checked first
+                    //FieldCopy.CopyFieldValues(new Uri(tfsCollectionUri), tfsProjectNames, tfsWorkItemTypes, fromField, toField, false, val => val != null && long.Parse(val.ToString()) <= 1000);
                 }
                 else
                 {
-                    Console.WriteLine("Cancelled!");
+                    Console.WriteLine("Team Project Collection Uri is not well-formed!");
                 }
-                Console.WriteLine("<ENTER> to exit?");
-                Console.ReadLine();
             }
             else
             {
-                Console.WriteLine("Wrong input");
+                Console.WriteLine("Cancelled!");
             }
+            Console.WriteLine("<ENTER> to exit?");
+            Console.ReadLine();
+
         }
     }
 }
